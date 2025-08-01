60b441b8da477be3a1dc24b16c620174141af7f0
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-01 09:48:16 -0600

    Sin excepciones

--- actividad12.py
+++ actividad12.py
@@ -10,9 +10,28 @@ def input_integer(message): #INGRESAR UN ENTERO Y VERIFICAR QUE SU ENTRADA SEA V
         except Exception as e: print('-'*50+'\n'+"✖"*5+"   Lo siento, ocurrió un error inesperado, intentelo nuevamente   "+f":{e}"+"✖"*5)
     return value
 
-while True:
-    n = input_integer(">Ingrese la catnidad de estudiantes")
-    if n > 0: break
+try:
+    while True:
+        n = input_integer(">Ingrese la catnidad de estudiantes: ")
+        if n > 0: break
+        else: print(error_mesagge)
 
-for i in range(n):
-    print("-"*10+f"ESTUDIANTE {i+1}"+"-"*10)
+    for i in range(n):
+        print("-"*10+f"ESTUDIANTE {i+1}"+"-"*10)
+        name = input(">Ingrese el nombre: ")
+        while True:
+            c_notes = input_integer(">Ingrese la catnidad de notas: ")
+            if c_notes > 0: break
+            else: print(error_mesagge)
+        notes = []
+        for j in range(c_notes):
+            note = input_integer(f"   >Ingrese nota {j+1}: ")
+            notes.append(note)
+
+        students[name] = notes
+    #CALCULAR PROMEDIOS
+    print(f"{'Nombre':<30}{'Promedio':<20}")
+    for names, values in students.items():
+        print(f"{names:<30}{sum(values)/len(values)}")
+except ZeroDivisionError: print("Lo sentimos, usted intentó sacar el promedio de cero notas")
+except Exception as
