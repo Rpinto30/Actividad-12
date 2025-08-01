e531fb830ac07dcc958b298bfaa0faf16e1844a2
Rpinto30 <rodrigopinto3007@gmail.com>
2025-08-01 09:51:27 -0600

    Programa terminado

--- actividad12.py
+++ actividad12.py
@@ -14,6 +14,7 @@ try:
     while True:
         n = input_integer(">Ingrese la catnidad de estudiantes: ")
         if n > 0: break
+        elif n == 0: print("Lo sentimos, debe de ingresar una cantidad de estudiantes mayor a 0")
         else: print(error_mesagge)
 
     for i in range(n):
@@ -22,6 +23,7 @@ try:
         while True:
             c_notes = input_integer(">Ingrese la catnidad de notas: ")
             if c_notes > 0: break
+            elif c_notes == 0: print("Lo sentimos, debe de ingresar una cantidad de notas mayor a 0")
             else: print(error_mesagge)
         notes = []
         for j in range(c_notes):
@@ -34,4 +36,4 @@ try:
     for names, values in students.items():
         print(f"{names:<30}{sum(values)/len(values)}")
 except ZeroDivisionError: print("Lo sentimos, usted intentó sacar el promedio de cero notas")
-except Exception as
+except Exception as e: print("Lo sentimos, ocurrió un error inesperado")
